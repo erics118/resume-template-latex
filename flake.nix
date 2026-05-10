19ca225f1826c56fb81070565f4679b826b19a30
erics118 <52634785+erics118@users.noreply.github.com>
2026-05-10 17:58:33 -0400

    update template
    increase font size
    add more spacing for bullets

--- flake.nix
+++ flake.nix
@@ -3,31 +3,38 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+    flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
   outputs =
-    { nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-      in
-      {
-        devShells.default = pkgs.mkShell {
-          packages = with pkgs; [
-            (texlive.combine {
-              inherit (texlive) scheme-full;
-            })
-            texlab
-            perl
-            perlPackages.FileHomeDir
-            perlPackages.UnicodeLineBreak
-            perlPackages.YAMLTiny
-            perlPackages.LogLog4perl
-            perlPackages.LogDispatch
-          ];
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+
+      perSystem =
+        { pkgs, ... }:
+        {
+          devShells.default = pkgs.mkShell {
+            packages = with pkgs; [
+              (texlive.combine {
+                inherit (texlive) scheme-full;
+              })
+              texlab
+              perl
+              nil
+              nodejs
+              perlPackages.FileHomeDir
+              perlPackages.UnicodeLineBreak
+              perlPackages.YAMLTiny
+              perlPackages.LogLog4perl
+              perlPackages.LogDispatch
+            ];
+          };
         };
-      }
-    );
+    };
 }
